99eafaa6927d503ea57f8e321b36f5ecb57a5b92
thuang2 <thuang2@cisco.com>
2018-07-23 14:29:43 +0800

    v1.3 in django

--- compare/web/form.py
+++ compare/web/form.py
@@ -2,7 +2,11 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    title1 = forms.CharField(max_length=50)
-    title2 = forms.CharField(max_length=50)
-    file1 = forms.FileField()
-    file2 = forms.FileField()
+    title1 = forms.CharField(max_length=50, required=False)
+    title2 = forms.CharField(max_length=50, required=False)
+    title3 = forms.CharField(max_length=50, required=False)
+    title4 = forms.CharField(max_length=50, required=False)
+    file1 = forms.FileField( required=False)
+    file2 = forms.FileField( required=False)
+    file3 = forms.FileField( required=False)
+    file4 = forms.FileField( required=False)
