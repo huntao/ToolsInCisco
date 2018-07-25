2330455a154e4128bc62acf60fc5808b89534634
thuang2 <thuang2@cisco.com>
2018-07-25 21:58:14 +0800

    v1.4 in django All

--- compare/compare/urls.py
+++ compare/compare/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from web.views import upload_file, compare
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('compare/', upload_file),
+    re_path('compare/(?P<num1>\d+)/(?P<num2>\d+)/', upload_file),
     path('result/', compare),
 ]
--- compare/web/views.py
+++ compare/web/views.py
@@ -6,10 +6,8 @@ import re
 from bs4 import BeautifulSoup
 # Create your views here.
 
-x1 = ''
-x2 = ''
-x3 = ''
-x4 = ''
+xa = []
+xb = []
 def handle_uploaded_file(f, k):
     #f = UploadFileFormt(request.POST, request.FILES)
     with open('upload/%s.html' % k, 'wb+') as destination:
@@ -17,26 +15,33 @@ def handle_uploaded_file(f, k):
             destination.write(chunk)
 
 
-def upload_file(request):
+def upload_file(request,num1, num2):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
-        print(request.__dir__())
+        num_1 = int(num1)+1
+        num_2 = int(num2)+1
         if form.is_valid():
+            global xa, xb
             #for a in request.FILES:
-
-            handle_uploaded_file(request.FILES['file1'], request.POST['title1'])
-            handle_uploaded_file(request.FILES['file2'], request.POST['title2'])
-            handle_uploaded_file(request.FILES['file3'], request.POST['title3'])
-            handle_uploaded_file(request.FILES['file4'], request.POST['title4'])
-            global x1,x2,x3,x4
-            x1 = request.POST['title1']
-            x2 = request.POST['title2']
-            x3 = request.POST['title3']
-            x4 = request.POST['title4']
+            for i in range(1,num_1):
+                for j in range(1,num_2):
+                    handle_uploaded_file(request.FILES['file_a%d' % i], request.POST['title_a%d' % i])
+                    handle_uploaded_file(request.FILES['file_b%d' % j], request.POST['title_b%d' % j])
+                    t1 = request.POST['title_a%d' % i ]
+                    t2 = request.POST['title_b%d' % j ]
+                    print(t1)
+                    print(t2)
+                    print(type(xa))
+                    xa.append(request.POST['title_a%d' % i ])
+                    xb.append(request.POST['title_b%d' % j ])
+            # x1 = request.POST['title1']
+            # x2 = request.POST['title2']
+            # x3 = request.POST['title3']
+            # x4 = request.POST['title4']
             return redirect('/result/')
     else:
         form = UploadFileForm()
-    return render_to_response('init.html', {'form': form})
+    return render_to_response('init%s-%s.html' % (num1,num2), {'form': form})
 
 
 def error_flag(self):
@@ -71,29 +76,47 @@ def compare_dict(result_1, result_2):
     differences = {}
     for key in result_2.keys():
         if key in result_1.keys():
-            if result_2[key] != '*.Pass</a':
+            if result_2[key] != 'Pass</a':
                 differences[key]=[result_1[key], result_2[key]]
     return differences
 
 
 def compare(request):
-    global x1, x2, x3, x4
-    file1 = 'upload/%s.html' % x1
-    file2 = 'upload/%s.html' % x2
-    file3 = 'upload/%s.html' % x3
-    file4 = 'upload/%s.html' % x4
-    soup_1 = BeautifulSoup(open(file1), 'lxml')
-    soup_2 = BeautifulSoup(open(file2), 'lxml')
-    soup_3 = BeautifulSoup(open(file3), 'lxml')
-    soup_4 = BeautifulSoup(open(file4), 'lxml')
-    table_1 = create_table(soup_1)
-    table_2 = create_table(soup_2)
-    table_3 = create_table(soup_3)
-    table_4 = create_table(soup_4)
-    table_a = table_1 + table_2
-    table_b = table_3 + table_4
+    global xa, xb
+    table_a = []
+    table_b = []
+    for x in xa:
+        file = 'upload/%s.html' % x
+        soup = BeautifulSoup(open(file), 'lxml')
+        table = create_table(soup)
+        table_a += table
+
+    for y in xb:
+        file = 'upload/%s.html' % y
+        soup = BeautifulSoup(open(file), 'lxml')
+        table = create_table(soup)
+        table_b += table
     dict_1 = list_to_dict(table_a)
     dict_2 = list_to_dict(table_b)
     dict_content = compare_dict(dict_1, dict_2)
     print(dict_content)
     return render_to_response('result.html', {'data': dict_content})
+    # file1 = 'upload/%s.html' % x1
+    # file2 = 'upload/%s.html' % x2
+    # file3 = 'upload/%s.html' % x3
+    # file4 = 'upload/%s.html' % x4
+    # soup_1 = BeautifulSoup(open(file1), 'lxml')
+    # soup_2 = BeautifulSoup(open(file2), 'lxml')
+    # soup_3 = BeautifulSoup(open(file3), 'lxml')
+    # soup_4 = BeautifulSoup(open(file4), 'lxml')
+    # table_1 = create_table(soup_1)
+    # table_2 = create_table(soup_2)
+    # table_3 = create_table(soup_3)
+    # table_4 = create_table(soup_4)
+    # table_a = table_1 + table_2
+    # table_b = table_3 + table_4
+    # dict_1 = list_to_dict(table_a)
+    # dict_2 = list_to_dict(table_b)
+    # dict_content = compare_dict(dict_1, dict_2)
+    # print(dict_content)
+    # return render_to_response('result.html', {'data': dict_content})
